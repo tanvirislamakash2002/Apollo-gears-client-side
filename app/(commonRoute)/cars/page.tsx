7b5311bedf46ecfd374a1d7205e2c64c02f90a49
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-08-19 15:49:25 +0600

    feat: refactor car data fetching to use fetchCars function and simplify component structure

--- app/(commonRoute)/cars/page.tsx
+++ app/(commonRoute)/cars/page.tsx
@@ -1,35 +1,22 @@
-
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import React from 'react'
-import TotalProduct from './TotalProduct'
+import { fetchCars } from "@/lib/fetchCars";
 
 export async function generateMetadata() {
-  const data = await fetch("http://localhost:5000/api/v1/cars")
-  const cars = await data.json()
+  const cars = await fetchCars()
+
   return {
-    title: `Cars - ${cars?.data?.length || 0} cars available`,
-    description: "Browse our collection of cars"
+    title: `Cars - ${cars?.data?.length || 0} cars available`
   }
 }
-export default async function page() {
-  const data = await fetch("http://localhost:5000/api/v1/cars")
-  const cars = await data.json()
-  console.log(cars)
+
+export default async function Page() {
+  const cars = await fetchCars()
+
   return (
-    <div className='p-4 rounded'>
+    <div>
       <h1>Cars</h1>
-      <ul className='border'>
-        {cars?.data?.map((car: any) => (
-          <div key={car.id}>
-            <li>{car.make}{car.model}</li>
-            <Button>
-              <Link href={`/cars/${car?.id}`}>view Details</Link>
-            </Button>
-          </div>
-        ))}
-      </ul>
-      <TotalProduct />
+      {cars?.data?.map((car: any) => (
+        <div key={car.id}>{car.make}</div>
+      ))}
     </div>
   )
 }
