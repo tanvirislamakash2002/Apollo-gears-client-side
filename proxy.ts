4832864741d8d228c8918ad46ab6196e27338cdb
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-08-20 21:35:52 +0600

    feat: add JWT verification in proxy middleware and handle token expiration

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,23 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextResponse, NextRequest } from 'next/server'
 
 const publicRoutes = ['/', '/about', '/contact', '/login', '/signup']
 export async function proxy(request: NextRequest) {
     const { pathname } = request.nextUrl;
     const accessToken = request.cookies.get('accessToken')?.value;
-    console.log(accessToken)
+    let userRole = null;
+    if (accessToken) {
+        try {
+            const decode = jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET as string) as JwtPayload
+
+            userRole = decode.role
+        } catch (error) {
+            const res = NextResponse.redirect(new URL("/login", request.url))
+            res.cookies.delete("accessToken")
+            res.cookies.delete("refreshToken")
+            return res
+        }
+    }
 
     return NextResponse.next()
 }
